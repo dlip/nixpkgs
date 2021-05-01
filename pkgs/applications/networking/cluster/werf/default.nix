e313c30e8366df46c6a550228217aef7d60cb2c8
Dane Lipscombe <dane@lipscombe.com.au>
2021-05-01 11:14:35 +1000

    .

--- pkgs/applications/networking/cluster/werf/default.nix
+++ pkgs/applications/networking/cluster/werf/default.nix
@@ -5,12 +5,12 @@ buildGoPackage rec {
   version = "1.2.10";
 
   goPackagePath = "github.com/werf/werf";
-  subPackages = [ "./cmd/werf/main.go" ];
+  subPackages = [ "cmd/werf" ];
 
   src = fetchFromGitHub {
     owner = "werf";
     repo = "werf";
     rev = "5af62f45a0df51b9af10fb8477392bd7236a208d";
-    sha256 = "";
+    sha256 = "t0mETZdxaA4J0yHVZ+3A/9BUZ+L9hBNkDMGNQOgAg/A=";
   };
 }
