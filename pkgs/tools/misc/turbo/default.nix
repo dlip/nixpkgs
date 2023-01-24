f5ddb9a0d5131c88ecc85fd2578d1fbb795d72be
Dane Lipscombe <danelipscombe@gmail.com>
2023-01-24 14:23:49 +1100

    Try go-turbo

--- pkgs/tools/misc/turbo/default.nix
+++ pkgs/tools/misc/turbo/default.nix
@@ -9,17 +9,18 @@
 }:
 
 buildGoModule rec {
-  pname = "turbo";
-  version = "1.6.3";
+  pname = "go-turbo";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "vercel";
     repo = "turbo";
     rev = "v${version}";
-    sha256 = "csapIeVB0FrLnmtUmLrRe8y54xmK50X30CV476DXEZI=";
+    sha256 = "YTuEv2S3jNV2o7HJML+P6OMazgwgRhUPnd/zaTWfDWs=";
   };
 
   modRoot = "cli";
+  doCheck = false;
 
   vendorSha256 = "Kx/CLFv23h2TmGe8Jwu+S3QcONfqeHk2fCW1na75c0s=";
 
@@ -45,6 +46,16 @@ buildGoModule rec {
     git config --global init.defaultBranch main
     git init
     popd
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    dir="$GOPATH/bin"
+    [ -e "$dir" ] && cp $dir/turbo $out/bin/${pname}
+
+    runHook postInstall
   '';
 
   meta = with lib; {
